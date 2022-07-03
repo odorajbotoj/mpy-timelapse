6bdc39073db1858355f187c942deed1d5d2cc92d
薛子豪 <odoraj_botoj@tom.com>
2022-07-03 17:50:22 +0800

    Update to Ver. 2

--- boot.py
+++ boot.py
@@ -1,6 +1,18 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
+# mpy-timelapse
+# Ver. 2
+# For: ESP 32 CAM
+# By: odorajbotoj
+# Update time: 2022-07-03
 import camera, uos, utime
 from machine import SDCard, Pin
+
+########
+# 休眠时长
+# 注意拍照和保存会耗掉大约1s
+SLEEP_TIME = 4 # (s)
+########
+
 led = Pin(33, Pin.OUT) # 状态指示灯 板载LED
 led(0) # on
 utime.sleep(0.5)
@@ -9,22 +21,15 @@ try:
     camera.init(0, format=camera.JPEG) # 初始化
     camera.framesize(camera.FRAME_UXGA) # 1600*1200
     camera.quality(10) # 高品质
-    utime.sleep(1) # 等待完全启动
+    utime.sleep(2) # 等待完全启动
 except:
     print("Camera Init ERROR")
     for i in range(25):
-        led(1)
-        utime.sleep_ms(150)
-        led(0)
-        utime.sleep_ms(150)
-        led(1)
-        utime.sleep_ms(150)
-        led(0)
-        utime.sleep_ms(150)
-        led(1)
-        utime.sleep_ms(150)
-        led(0)
-        utime.sleep_ms(150)
+        for j in range(3):
+            led(0)
+            utime.sleep_ms(150)
+            led(1)
+            utime.sleep_ms(150)
         led(1)
         utime.sleep_ms(1100)
         
@@ -33,14 +38,11 @@ try:
 except:
     print("SD Card ERROR")
     for i in range(25):
-        led(1)
-        utime.sleep_ms(250)
-        led(0)
-        utime.sleep_ms(250)
-        led(1)
-        utime.sleep_ms(250)
-        led(0)
-        utime.sleep_ms(250)
+        for j in range(2):
+            led(0)
+            utime.sleep_ms(250)
+            led(1)
+            utime.sleep_ms(250)
         led(1)
         utime.sleep_ms(1000)
 uos.chdir("sd") # 更改工作目录
@@ -67,28 +69,17 @@ while 1:
     except:
         print("Take photos and save ERROR")
         for i in range(25):
-            led(1)
-            utime.sleep_ms(100)
-            led(0)
-            utime.sleep_ms(100)
-            led(1)
-            utime.sleep_ms(100)
-            led(0)
-            utime.sleep_ms(100)
-            led(1)
-            utime.sleep_ms(100)
-            led(0)
-            utime.sleep_ms(100)
-            led(1)
-            utime.sleep_ms(100)
-            led(0)
-            utime.sleep_ms(100)
+            for j in range(4):
+                led(0)
+                utime.sleep_ms(100)
+                led(1)
+                utime.sleep_ms(100)
             led(1)
             utime.sleep_ms(1200)
     print("Success")
-    for i in range(4):
+    for i in range(SLEEP_TIME):
         led(0)
         utime.sleep_ms(100)
         led(1)
         utime.sleep_ms(900)
-    print("Slept 4s")
+    print("Slept "+str(SLEEP_TIME)+"s")
